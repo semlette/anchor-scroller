2bd3c489f87c67618418c558e6e74ab55f4f7a60
Andi Semler <andirobinsemler@gmail.com>
2017-02-22 12:48:44 +0100

    Use `rollup.targets`

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,14 @@ import babel from 'rollup-plugin-babel';
 import typescript from 'rollup-plugin-typescript';
 import latestTypescript from 'typescript';
 
-const format = process.env.FORMAT;
-
 export default {
-  entry: `${__dirname}/source/AnchorScroller.ts`,
-  dest: `${__dirname}/distribution/AnchorScroller${ process.env.FORMAT === 'umd' ? '' : `.${format}` }.js`,
+  entry: 'source/AnchorScroller.ts',
+  targets: [
+    { format: 'es', dest: 'distribution/AnchorScroller.es.js' },
+    { format: 'cjs', dest: 'distribution/AnchorScroller.cjs.js' },
+    { format: 'umd', dest: 'distribution/AnchorScroller.js' }
+  ],
   moduleName: 'AnchorScroller',
-  format,
   sourceMap: true,
   exports: 'default',
   plugins: [
